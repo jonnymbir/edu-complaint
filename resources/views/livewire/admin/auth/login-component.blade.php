bd515a8ec869b9bbb0d8a22035e38cbd618ecd45
Favour Nwevo <favour@rxhealthinfosystems.com>
2023-08-27 13:53:07 +0000

    remove logo from login and .env from ignore

--- resources/views/livewire/admin/auth/login-component.blade.php
+++ resources/views/livewire/admin/auth/login-component.blade.php
@@ -9,7 +9,7 @@
 				<div class="row">
 					<div class="col-xl-7 col-xxl-8">
 						<a href="/" class="text-nowrap logo-img d-block px-4 py-9 w-100">
-							<img src="{{ asset('dist/images/favicon.png') }}" width="100" alt="">
+{{--							<img src="{{ asset('dist/images/favicon.png') }}" width="100" alt="">--}}
 						</a>
 						<div class="d-none d-xl-flex align-items-center justify-content-center" style="height: calc(100vh - 80px);">
 {{--							<img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/backgrounds/login-security.svg" alt="" class="img-fluid" width="500">--}}
@@ -20,7 +20,7 @@
 						<div class="authentication-login min-vh-100 bg-body row justify-content-center align-items-center p-4">
 							<div class="col-sm-8 col-md-6 col-xl-9">
 								<h2 class="mb-3 fs-7 fw-bolder">Welcome to {{ config('app.name') }}</h2>
-								<p class=" mb-9">Your Admin Dashboard</p>
+{{--								<p class=" mb-9">Your Admin Dashboard</p>--}}
 								<form>
 									<div class="mb-3">
 										<label for="email" class="form-label">Email Address</label>
