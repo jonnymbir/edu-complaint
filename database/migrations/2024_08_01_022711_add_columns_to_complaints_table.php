db0fae753fbb0787d96a9e1be3755af655ac5ab9
OmegaSL <favournwevo@gmail.com>
2024-08-04 13:34:42 +0000

    feat: added new columns to complaints table

---
+++ database/migrations/2024_08_01_022711_add_columns_to_complaints_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('complaints', function (Blueprint $table) {
+			$table->string('town_locality')->nullable()->after('district_id');
+			$table->string('school')->nullable()->after('town_locality');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('complaints', function (Blueprint $table) {
+            $table->dropColumn('town_locality');
+            $table->dropColumn('school');
+        });
+    }
+};
